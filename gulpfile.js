8a94cace06edfe113609d5290f1dc62d3e0a0e52
Jimmy Letecheur <jimmy@letecheur.me>
2016-09-21 12:03:44 +0200

    add workflow for images

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,36 @@
 // Définition dependancy for execute tasks
 var
-  gulp = require('gulp');
+  gulp = require('gulp'),
+  imagemin = require('gulp-imagemin'),
+  newer = require('gulp-newer'),
+  size = require('gulp-size'),
+  del = require('del');
 
 // Definition generals variables for our gupfiles
 var
   source = 'source/',
   dest = 'build/';
 
-// Definition variables for tasls (options tasks)
+// Definition variables for taslk (options tasks)
 var
   imageOptions = {
-
+    in: source + 'images/*.*',
+    out: dest + 'images/'
   };
 
+
 // Task definitions
+gulp.task('clean', function(){
+  del( [dest + '*'] );
+});
+
 gulp.task('images', function(){
+  gulp.src(imageOptions.in) // Prendre les fichiers dans imageOptions.in
+    .pipe(newer(imageOptions.out)) // Pour vérifier si il y a du nouveau dans le dossier
+    .pipe(size({title: 'Images size before compression: ', showFiles: true}))
+    .pipe(imagemin())
+    .pipe(size({title: 'Images size after compression: ', showFiles: true}))
+    .pipe(gulp.dest(imageOptions.out)); // .pipe pour enchainer les actions, donner le chemin de destination
 
 });
 
