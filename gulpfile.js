e93f772ae483c5783b38bc3702cac5ef80fc8839
Jimmy Letecheur <jimmy@letecheur.me>
2016-09-21 11:22:51 +0200

    Add dependencie imagemin and comments generals

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
+// Définition dependancy for execute tasks
 var
   gulp = require('gulp');
 
-gulp.task('default', function(){
-  
+// Definition generals variables for our gupfiles
+var
+  source = 'source/',
+  dest = 'build/';
+
+// Definition variables for tasls (options tasks)
+var
+  imageOptions = {
+
+  };
+
+// Task definitions
+gulp.task('images', function(){
+
 });
 
+// Tâche par défaut exécutée lorsqu'on tape juste gulp dans le terminal
 gulp.task('default', function(){
-
+  imagemin = require('');
 });
